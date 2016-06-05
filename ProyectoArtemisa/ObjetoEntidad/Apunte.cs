e2f173794039335759fde1e26ecc7c56c3bb6a32
SebaPucheta <seba.pucheta.17@gmail.com>
2016-06-05 17:53:56 -0300

    Proyecto_ProyectoArtemisa_v0.0.1

---
+++ ProyectoArtemisa/ObjetoEntidad/Apunte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetoEntidad
+{
+    public class Apunte
+    {
+        public string codigoBarraApunte { get; set; }
+        public int stock { get; set; }
+        public double precio { get; set; }
+        public int cantHoja { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int ano { get; set; }
+        public int idPrecioHoja { get; set; }
+        public int idCategoria { get; set; }
+        public int idTipoApunte { get; set; }
+        public int idEditorial { get; set; }
+        public int idEstado { get; set; }
+        public int idProfesor { get; set; }
+    }
+}
--- ProyectoArtemisa/ObjetoEntidad/Editorial-.cs
+++ ProyectoArtemisa/ObjetoEntidad/Editorial-.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ObjetoEntidad
 {
-    public class Class1
+    class Editorial_
     {
     }
 }
