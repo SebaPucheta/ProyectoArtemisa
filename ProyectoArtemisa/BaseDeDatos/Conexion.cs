d6f505162d770545ec763ff276a17eea09b932c7
Juan Moyano <juan.moyano.94@gmail.com>
2016-06-11 16:10:45 -0300

    Uso de Summary
    Poner "///" para que aparezca el summary y comentar los metodos

--- ProyectoArtemisa/BaseDeDatos/Conexion.cs
+++ ProyectoArtemisa/BaseDeDatos/Conexion.cs
@@ -11,8 +11,13 @@ namespace BaseDeDatos
 {
     public class Conexion
     {
+        /// <summary> obtenerBD es un método de la clase Conexion
+        /// para abrir la conexion con la base de datos
+        /// </summary>
+        /// <returns>SqlConnection con la base de datos abierta</returns>
         protected static SqlConnection obtenerBD()
         {
+
             string stringConexion = "Data Source=192.168.1.12,49170;Initial Catalog=EDUCOM;Persist Security Info=True;User ID=sa;Password=artemisa";
             SqlConnection con = new SqlConnection(stringConexion);
             con.Open();
