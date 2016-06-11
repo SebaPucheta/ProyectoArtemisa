976ae122ec492efe5e6ec29c426ad67fbf54520d
Martin <martin-u@live.com>
2016-06-11 15:26:32 -0300

    Conexion_ProyectoArtemisa_v0.0.4
    Creada la conexion fantasma de martin

---
+++ ProyectoArtemisa/BaseDeDatos/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BaseDeDatos
+{
+    public class Conexion
+    {
+        protected static SqlConnection obtenerBD()
+        {
+            string stringConexion = "Data Source=192.168.1.12,49170;Initial Catalog=EDUCOM;Persist Security Info=True;User ID=sa;Password=artemisa";
+            SqlConnection con = new SqlConnection(stringConexion);
+            con.Open();
+            return con;
+        }
+    }
+}
