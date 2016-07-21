a9cb0d34307b6b516548afd46361a6038c6f999d
SebaPucheta <seba.pucheta.17@gmail.com>
2016-07-21 16:16:08 -0300

    ProyectoArtemisa_v0.0.36

--- ProyectoArtemisa/ObjetoEntidad/DetalleFacturaEntidadQuery.cs
+++ ProyectoArtemisa/ObjetoEntidad/DetalleFacturaEntidadQuery.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public  class DetalleFacturaEntidadQuery
+    public  class DetalleFacturaEntidadQuery : DetalleFacturaEntidad
     {
         public string nombreApunte { get; set; }
     }
--- ProyectoArtemisa/ObjetoEntidad/OrdenImpresionEntidadQuery.cs
+++ ProyectoArtemisa/ObjetoEntidad/OrdenImpresionEntidadQuery.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class OrdenImpresionEntidadQuery
+    public class OrdenImpresionEntidadQuery : OrdenImpresionEntidad
     {
         public string nombreApunte { get; set; }
     }
