016af4add7c83b6e84ce0f446b6e59b0a06c7722
martin-u <uri.mar09@gmail.com>
2016-08-13 19:36:01 -0300

    v0.0.46

--- ProyectoArtemisa/ProyectoArtemisa/ConsultarHistorialFactura_130.aspx.cs
+++ ProyectoArtemisa/ProyectoArtemisa/ConsultarHistorialFactura_130.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Entidades;
+using System.Data;
+using BaseDeDatos;
+
 
 namespace ProyectoArtemisa
 {
@@ -11,11 +15,44 @@ namespace ProyectoArtemisa
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                cargarGrillaFactura(FacultadDao.listarFacturas);
+            }
 
         }
         protected void btn_consultarFactura_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        protected void cargarGrillaFactura(List<FacturaEntidadQuery> listaFacturas)
+        {
+            DataTable tabla = new DataTable();
+            DataRow fila;
+
+            //Creo las columnas de la tabla
+            tabla.Columns.Add("numero", typeof(int));//key name
+            tabla.Columns.Add("fecha", typeof(DateTime));
+            tabla.Columns.Add("total", typeof(float));
+            tabla.Columns.Add("tipoPago", typeof(string));
+
+            foreach (FacturaEntidadQuery factura in listaFacturas)
+            {
+                fila = tabla.NewRow();
+
+                fila[0] = factura.idFactura;
+                fila[1] = factura.fecha;
+                fila[2] = factura.total;
+                fila[3] = factura.nombreTipoPago;
+                tabla.Rows.Add(fila);
+            }
+
+            DataView dataView = new DataView(tabla);
+
+            dgv_grillaOrdenesImpresion.DataSource = dataView;
+            dgv_grillaOrdenesImpresion.DataKeyNames = new string[] { "idFactura" };
+            dgv_grillaOrdenesImpresion.DataBind();
         }
     }
 }
